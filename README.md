da98d507f0ef3a7485b2b083306cf099471a36a9
Daniel <qiuzhanghua@icloud.com>
2020-05-05 07:24:33 +0800

    add redis

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Tide
 
-## Performance (12k qps)
+## Performance
+### Hello (127k qps)
 ```text
 ➜  ~ wrk -t12 -c200 -d30s http://127.0.0.1:8080
 Running 30s test @ http://127.0.0.1:8080
@@ -12,6 +13,18 @@ Running 30s test @ http://127.0.0.1:8080
   Socket errors: connect 0, read 19, write 0, timeout 0
 Requests/sec: 127529.45
 Transfer/sec:     12.77MB
+```
+### Redis(6.0 38k qps)
+```text
+➜  ~ wrk -t12 -c100 -d30s http://127.0.0.1:8080/redis
+Running 30s test @ http://127.0.0.1:8080/redis
+  12 threads and 100 connections
+  Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency     2.51ms    1.29ms  11.15ms   68.40%
+    Req/Sec     3.22k   127.83     3.61k    68.29%
+  1156472 requests in 30.10s, 116.91MB read
+Requests/sec:  38418.81
+Transfer/sec:      3.88MB
 ```
 
 ## Computer
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+#[macro_use]
+extern crate lazy_static;
+
+use r2d2::{Pool, PooledConnection};
 use tide::{Request, Response};
 
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
+    dotenv::dotenv().ok();
+    //    env_logger::init();
     let mut app = tide::new();
     app.at("/").get(hello);
+    app.at("/redis").get(redis);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
@@ -11,4 +18,46 @@ async fn main() -> Result<(), std::io::Error> {
 async fn hello(_req: Request<()>) -> tide::Result {
     //    Ok(Response::new(http_types::StatusCode::Ok).body_string("Tide".to_string()))
     Ok(Response::from("Tide"))
+}
+
+async fn redis(_req: Request<()>) -> std::result::Result<tide::Response, http_types::Error> {
+    use r2d2_redis::redis::Commands;
+    let mut conn = get_redis_connection();
+    match conn.get::<&str, String>("key") {
+        Ok(s) => Ok(Response::from(s)),
+        Err(e) => std::result::Result::Err(http_types::Error::new(
+            http_types::StatusCode::InternalServerError,
+            e,
+        )),
+    }
+}
+
+pub const REDIS_POOL_SIZE: u32 = 100;
+
+lazy_static! {
+    pub static ref REDIS_POOL: Pool<r2d2_redis::RedisConnectionManager> = {
+        dotenv::dotenv().ok();
+        let redis_url = std::env::var("REDIS_URL").expect("REDIS_URL must be set");
+        let manager = r2d2_redis::RedisConnectionManager::new(redis_url).unwrap();
+        r2d2::Pool::builder()
+            .max_size(REDIS_POOL_SIZE)
+            .build(manager)
+            .expect("Failed to create redis pool.")
+    };
+
+    // Used to update core data into redis master, such as person, role and dept etc.
+    // pub static ref MASTER_REDIS_POOL: Pool<r2d2_redis::RedisConnectionManager> = {
+    //     dotenv().ok();
+    //     let redis_url = env::var("MASTER_REDIS_URL").expect("MASTER_REDIS_URL must be set");
+    //     let manager = r2d2_redis::RedisConnectionManager::new(redis_url).unwrap();
+    //     r2d2::Pool::builder()
+    //         .max_size(REDIS_POOL_SIZE)
+    //         .build(manager)
+    //         .expect("Failed to create master redis pool.")
+    // };
+
+}
+
+pub fn get_redis_connection() -> PooledConnection<r2d2_redis::RedisConnectionManager> {
+    REDIS_POOL.clone().get().unwrap()
 }
