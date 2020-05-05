742e7abba5baa590d4fff89e719cb0ec0422d0f4
Daniel <qiuzhanghua@icloud.com>
2020-05-05 10:04:45 +0800

    add my/pg

--- README.md
+++ README.md
@@ -1,34 +1,66 @@
 # Tide
 
-## Performance
-### Hello (127k qps)
-```text
-➜  ~ wrk -t12 -c200 -d30s http://127.0.0.1:8080
-Running 30s test @ http://127.0.0.1:8080
-  12 threads and 200 connections
-  Thread Stats   Avg      Stdev     Max   +/- Stdev
-    Latency     3.24ms    9.05ms 150.37ms   95.23%
-    Req/Sec    10.68k   678.75    12.72k    75.75%
-  3838891 requests in 30.10s, 384.41MB read
-  Socket errors: connect 0, read 19, write 0, timeout 0
-Requests/sec: 127529.45
-Transfer/sec:     12.77MB
-```
-### Redis(6.0 38k qps)
-```text
-➜  ~ wrk -t12 -c100 -d30s http://127.0.0.1:8080/redis
-Running 30s test @ http://127.0.0.1:8080/redis
-  12 threads and 100 connections
-  Thread Stats   Avg      Stdev     Max   +/- Stdev
-    Latency     2.51ms    1.29ms  11.15ms   68.40%
-    Req/Sec     3.22k   127.83     3.61k    68.29%
-  1156472 requests in 30.10s, 116.91MB read
-Requests/sec:  38418.81
-Transfer/sec:      3.88MB
+## Build
+```bash
+DATABASE_URL="mysql://app:app@localhost:3306/app" cargo build --release
+target/release/tide_web
 ```
 
-## Computer
+## Performance
+
+QPS = query per second
+
+| Content    | QPS  |
+|:-----------|:-----|
+| Hello      | 100k |
+| Redis(6.0) | 28k  |
+| MySQL      | 30k  |
+| PostgreSQL | 26k  |
+
+```text
+➜  ~ wrk -t12 -c200 -d30s http://127.0.0.1:8080/
+Running 30s test @ http://127.0.0.1:8080/
+  12 threads and 200 connections
+  Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency     1.96ms  200.41us  14.83ms   96.16%
+    Req/Sec     8.21k   151.16    10.33k    94.71%
+  2948214 requests in 30.10s, 295.22MB read
+Requests/sec:  97940.85
+Transfer/sec:      9.81MB
+➜  ~ wrk -t12 -c200 -d30s http://127.0.0.1:8080/redis
+Running 30s test @ http://127.0.0.1:8080/redis
+  12 threads and 200 connections
+  Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency     6.85ms    1.95ms  30.12ms   65.19%
+    Req/Sec     2.35k   129.43     5.54k    98.06%
+  842203 requests in 30.10s, 85.14MB read
+Requests/sec:  27978.33
+Transfer/sec:      2.83MB
+➜  ~ wrk -t12 -c200 -d30s http://127.0.0.1:8080/my
+Running 30s test @ http://127.0.0.1:8080/my
+  12 threads and 200 connections
+  Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency     7.65ms    9.34ms 155.52ms   95.07%
+    Req/Sec     2.49k   150.94     3.07k    71.31%
+  893050 requests in 30.01s, 91.13MB read
+Requests/sec:  29761.48
+Transfer/sec:      3.04MB
+➜  ~ wrk -t12 -c200 -d30s http://127.0.0.1:8080/pg
+Running 30s test @ http://127.0.0.1:8080/pg
+  12 threads and 200 connections
+  Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency     7.53ms    4.49ms 130.07ms   88.75%
+    Req/Sec     2.21k   102.87     2.51k    76.36%
+  792401 requests in 30.01s, 163.23MB read
+  Socket errors: connect 0, read 30, write 0, timeout 0
+Requests/sec:  26405.03
+Transfer/sec:      5.44MB
+```
+
+##  Env
+### Hardware
 ```text
 iMac(Retina 5K, 27-inch, 2019) CPU 3GHz, 6 Core i5
 Memory 32G 2667MHz DDR4
 ```
+
--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,29 @@
 extern crate lazy_static;
 
 use r2d2::{Pool, PooledConnection};
+use sqlx::prelude::*;
 use tide::{Request, Response};
 
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
     dotenv::dotenv().ok();
     //    env_logger::init();
-    let mut app = tide::new();
+    let db = DB::new().await;
+    let mut app = tide::with_state(db);
     app.at("/").get(hello);
     app.at("/redis").get(redis);
+    app.at("/my").get(my);
+    app.at("/pg").get(pg);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
 
-async fn hello(_req: Request<()>) -> tide::Result {
+async fn hello(_req: Request<DB>) -> tide::Result {
     //    Ok(Response::new(http_types::StatusCode::Ok).body_string("Tide".to_string()))
     Ok(Response::from("Tide"))
 }
 
-async fn redis(_req: Request<()>) -> std::result::Result<tide::Response, http_types::Error> {
+async fn redis(_req: Request<DB>) -> std::result::Result<tide::Response, http_types::Error> {
     use r2d2_redis::redis::Commands;
     let mut conn = get_redis_connection();
     match conn.get::<&str, String>("key") {
@@ -32,6 +36,35 @@ async fn redis(_req: Request<()>) -> std::result::Result<tide::Response, http_ty
     }
 }
 
+async fn my(req: Request<DB>) -> std::result::Result<tide::Response, http_types::Error> {
+    match sqlx::query!(r###"SELECT version() v"###)
+        .fetch_one(&req.state().my_pool)
+        .await
+        .map(|rec| rec.v)
+    {
+        Ok(s) => Ok(Response::from(s)),
+        Err(e) => std::result::Result::Err(http_types::Error::new(
+            http_types::StatusCode::InternalServerError,
+            e,
+        )),
+    }
+}
+
+async fn pg(req: Request<DB>) -> std::result::Result<tide::Response, http_types::Error> {
+    use sqlx_core::postgres::PgCursor;
+    let mut cursor: PgCursor = sqlx::query(r#"SELECT version() v"#).fetch(&req.state().pg_pool);
+    let row = cursor.next().await;
+    match row {
+        Ok(r) => Ok(Response::from(r.unwrap().get::<&str, &str>("v"))),
+        Err(e) => std::result::Result::Err(http_types::Error::new(
+            http_types::StatusCode::InternalServerError,
+            e,
+        )),
+    }
+}
+
+pub const MAX_POOL_SIZE: u32 = 64;
+pub const MIN_POOL_SIZE: u32 = 8;
 pub const REDIS_POOL_SIZE: u32 = 100;
 
 lazy_static! {
@@ -60,4 +93,39 @@ lazy_static! {
 
 pub fn get_redis_connection() -> PooledConnection<r2d2_redis::RedisConnectionManager> {
     REDIS_POOL.clone().get().unwrap()
+}
+
+pub struct DB {
+    my_pool: sqlx::MySqlPool,
+    pg_pool: sqlx::PgPool,
+}
+
+impl DB {
+    async fn new() -> DB {
+        let url = std::env::var("MYSQL_URL").expect("MYSQL_URL must be set");
+        let max_pool_size: u32 = std::env::var("MYSQL_MAX_POOL_SIZE")
+            .unwrap_or_else(|_| MAX_POOL_SIZE.to_string())
+            .parse::<u32>()
+            .unwrap_or(MAX_POOL_SIZE);
+        let min_pool_size: u32 = std::env::var("MYSQL_MIN_POOL_SIZE")
+            .unwrap_or_else(|_| MIN_POOL_SIZE.to_string())
+            .parse::<u32>()
+            .unwrap_or(MIN_POOL_SIZE);
+
+        let my_pool: sqlx::MySqlPool = sqlx::Pool::builder()
+            .max_size(max_pool_size)
+            .min_size(min_pool_size)
+            .build(&url)
+            .await
+            .unwrap();
+        let url = std::env::var("PG_URL").expect("PG_URL must be set");
+
+        let pg_pool: sqlx::PgPool = sqlx::Pool::builder()
+            .max_size(max_pool_size)
+            .min_size(min_pool_size)
+            .build(&url)
+            .await
+            .unwrap();
+        DB { my_pool, pg_pool }
+    }
 }
